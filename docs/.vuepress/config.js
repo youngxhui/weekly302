9fa6911fc173824b3810e1ac3597e291c2b28717
youngxhui <youngxhui@gmail.com>
2019-04-08 10:34:50 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -17,7 +17,7 @@ module.exports = {
     docsDir: 'docs',
     nav: [{ text: '主页', link: '/' }, { text: '2019周报', link: '/2019/' }],
     sidebar: 'auto',
-    sidebar: ['/', ['/2019/0325', '0325周报'], ['/2019/0401','0401周报'], ['/2019/0408','0408周报']],
+    sidebar: ['/', ['/2019/0325', '0325周报'], ['/2019/0401','0401周报']],
     sidebarDepth: 1,
     displayAllHeaders: true,
     lastUpdated: '最近更新',
